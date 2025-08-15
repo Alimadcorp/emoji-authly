650f9ed78e3cf686b26bbc26f1145600d02a19cd
Art3mis <pyatt.eve@gmail.com>
2025-08-15 17:29:51 +0000

    im so lost

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const { useSyncExternalStore } = require('react');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello'));
 app.listen(3000);
+app.use(express.json());
+app.post('/submit', (req, res) => {
+    const data = req.body;
+})
+
+const user = {}
+users['aaa'] = 'pass123';
+
+const hash = await bcrypt.hash('mpw', 10);
+
+users[username] = await bcrypt.hash(password, 10);
+
+await bcrypt.compare(password, users[username])
+
+req.session.userId = user.id;
+
+if (req.session.userId) {
+    res.render('profile');
+} else {
+    res.redirect('/login')
+}
+
+if (!req.session.userId) {
+    res.redirect('/login');
+}
